456bd566ee98d23b7fb92eb24eca45f1cdd3962a
teun.schriks <teun.schriks@moba.net>
2023-10-11 17:37:07 +0200

    formatting

--- client/src/LabelInfo.tsx
+++ client/src/LabelInfo.tsx
@@ -4,24 +4,31 @@ import React, {useState} from "react";
 interface Props {
     label: string
 }
+
 function LabelInfo({label}: Props) {
 
     const [labelInfo, setLabelInfo]: any = useState();
     React.useEffect(() => {
-        fetch("/labels?label="+label)
+        fetch("/labels?label=" + label)
             .then((res) => res.json())
             .then((d) => {
                 if (d) {
-                  console.log(d);
-                  setLabelInfo(d)
+                    console.log(d);
+                    setLabelInfo(d)
                 }
             }).catch((err) => console.log(err));
     }, [label]);
     return (
-        <div className="LabelInfo">
-            {labelInfo?.name\n}<br
-            {labelInfo?.eps\n}
-            {labelInfo?.mood\n}
+        <div  className="LabelInfo">
+            <div>
+                {labelInfo?.name}
+            </div>
+            <div>
+                {labelInfo?.eps}
+            </div>
+            <div>
+                {labelInfo?.mood}
+            </div>
         </div>
     );
 
